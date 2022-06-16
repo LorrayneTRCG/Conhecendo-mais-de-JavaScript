f8962feadf35baf65227d6a8bcdc04294c395c83
lorraynetrcosta@hotmail.com <lorraynetrcosta@hotmail.com>
2022-06-16 20:02:37 -0300

    Cria-se pasta e arquivos para aula 5

---
+++ aula5 - funçoes/script.js
@@ -0,0 +1,16 @@
+// função
+
+/* function funcao () {
+    console.log ('Tudo certo jovem!');
+}
+
+funcao ();
+
+// funções com parametros
+
+function mensagem(primeiro, segundo) {
+    console.log(primeiro, segundo)
+}
+ mensagem('tudo certo', 'jovem!'); */
+
+ 
