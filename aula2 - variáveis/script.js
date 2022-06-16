b487cb437d626f756adb9d6ac4f29bae721895c2
lorraynetrcosta@hotmail.com <lorraynetrcosta@hotmail.com>
2022-06-16 20:00:59 -0300

    Cria-se pasta e arquivos para aula 2

---
+++ aula2 - variáveis/script.js
@@ -0,0 +1,94 @@
+// Tipos primitivos //
+
+// boolean
+/* var vOuF = false
+console.log(typeof(vOuF));
+
+// number
+var numeroQrualquer = 1
+console.log(typeof(numeroQualquer));
+
+// string
+var nome = 'Lorrayne'
+console.log(typeof(nome)); */
+
+// como declarar
+/* var variavel = 'Lorrayne';
+variavel = 'martine';
+console.log(variavel);
+
+let variavel2 = 'Lorrayne';
+console.log(variavel2); */
+
+/*const constante = 'Lorrayne';
+console.log(constante); */
+
+/*var escopoGlobal = 'global';
+console.log(escopoGlobal);
+
+function escopoLocal() {
+    let escopoLocalInterno = 'local';
+    console.log(escopoLocalInterno);
+}
+
+escopoLocal(); */
+
+// atribuição
+/* var atribuição = 'Lorrayne' 
+
+// comparação
+var comparacao = '0' = 0;
+console.log(comparacao);
+
+// comparacao Identica
+var comparacaoIdentica = '0' === 0;
+console.log(comparacaoIdentica) */
+
+/* // adicao
+var adicao = 1 + 1;
+console.log(adicao); */
+
+/* // subtracao
+var subtracao = 2 - 1;
+console.log(subtracao); */
+
+/* // multiplicacao
+var multiplicacao = 2 * 3;
+console.log(multiplicacao); */
+
+/* // divisao Real
+var divisaoReal = 6 / 3;
+console.log(divisaoReal); */
+
+/* // divisao inteira
+var divisaoInteira = 5 % 2;
+console.log(divisaoInteira); */
+
+/* // potenciacao
+var potenciacao = 2 ** 10;
+console.log(potenciacao); */
+
+/* // maior que
+var maiorQue = 5 > 2;
+console.log(maiorQue); */
+
+/* // menor que
+var menorQue = 5 < 2;
+console.log(menorQue); */
+
+/* // maior ou igual
+var maiorOuIgual = 5 >=2;
+console.log(maiorOuIgual); */
+
+/* // menor ou igual
+var menorOuIgual= 5 <= 2;
+console.log(menorOuIgual); */
+
+/* var e =true && true;
+console.log(e); */
+
+/* var ou = false || false;
+console.log(ou);
+
+var nao = !true;
+console.log(nao);*/
